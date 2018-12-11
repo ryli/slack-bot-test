072fdf77fb24099c1c6639891a0c8ba308e672a2
root <root@vultr.guest>
2018-12-11 08:18:13 +0000

    feat: message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const Router = require('koa-router')
 const baselogger = require('koa-base-logger')
+const request = require('request')
 
 const app = new Koa()
 const router = new Router()
@@ -12,6 +13,7 @@ app.use(baselogger({
   appName: 'slack-bot',
   fileName: 'slack-bot',
   useKoa: true,
+  recordBody: true,
 }))
 
 router.get('/', (ctx, next) => {
@@ -22,11 +24,14 @@ router.get('/', (ctx, next) => {
 router.post('/', (ctx, next) => {
   const payload = ctx.request.body
   console.log('body: ', payload)
-  let responseText = 'default'
+  let responseText 
 
-  if (payload.event.type === "app_mention") {
+  if (payload.type === "url_verification") {
+    responseText = `challenge=${payload.challenge}`
+  } else if (payload.event.type === "app_mention") {
     if (payload.event.text.includes("tell me a joke")) {
       // Make call to chat.postMessage using bot's token
+      postMsg('joke')
     }
   } else if (payload.event.type === "message") {
     if (payload.event.text.includes("Who's there?")) {
@@ -37,6 +42,7 @@ router.post('/', (ctx, next) => {
 
     if (responseText !== undefined) {
       // Make call to chat.postMessage sending response_text using bot's token
+      postMsg(responseText)
     }
   }
 
@@ -50,3 +56,13 @@ app
   .use(router.allowedMethods())
 
 app.listen(3000)
+
+function postMsg(msg, opt) {
+  request.post(`https://slack.com/api/chat.postMessage`, {
+	  headers : { Authorization: 'Bearer xoxp-492044269362-491875545588-500459323636-c6ab8fe331c1279b34f99e8a7997057a'},
+    formData: {
+      text: msg,
+      channel: 'eshop' 
+    }
+  })
+}
